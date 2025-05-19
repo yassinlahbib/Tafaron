563296a97cb36f0902c3f9349f62ee54f8d77220
yassinlahbib <yaaston94@gmail.com>
2025-05-19 19:31:36 +0200

    Premiere partie initialisation Firebase et connexion user

---
+++ Tafaron/Authentication/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  Tafaron
+//
+//  Created by Yassin Lahbib on 19/05/2025.
+//
+
+import Foundation
---
+++ Tafaron/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Tafaron
+//
+//  Created by Yassin Lahbib on 19/05/2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
