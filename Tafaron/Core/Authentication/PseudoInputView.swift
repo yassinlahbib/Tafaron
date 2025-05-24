e1f5206bb041b3d0a16d1be4c5a35b613a7ad4b7
yassinlahbib <yaaston94@gmail.com>
2025-05-24 17:17:09 +0200

    ajout de demande de pseudo a l'inscription (pseudo pour permettre de faire des demande d'amis)

---
+++ Tafaron/Core/Authentication/PseudoInputView.swift
@@ -0,0 +1,18 @@
+//
+//  PseudoInputView.swift
+//  Tafaron
+//
+//  Created by Yassin Lahbib on 24/05/2025.
+//
+
+import SwiftUI
+
+struct PseudoInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PseudoInputView()
+}
